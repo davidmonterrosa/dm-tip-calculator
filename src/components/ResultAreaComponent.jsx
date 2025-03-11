614a822b6824d697b8c97ef6194c10f7ad0ac60d
davidmonterrosa <dmonterrosa99@gmail.com>
2025-03-11 14:52:47 -0700

    added fonts

--- src/components/ResultAreaComponent.jsx
+++ src/components/ResultAreaComponent.jsx
@@ -2,7 +2,20 @@ import React from 'react'
 
 const ResultAreaComponent = () => {
   return (
-    <div className='bg-strongCyan'>
+    <div className='bg-strongCyan rounded-2xl'>
+      <article>
+        <div>
+          <h1 className='text-white '>T</h1>
+          <p>/ person</p>
+        </div>
+      </article>
+      <article>
+        <div>
+          <h1 className='text-white '>T</h1>
+          <p>/ person</p>
+        </div>
+      </article>
+      <button></button>
 
     </div>
   )
